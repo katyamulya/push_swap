20f33838db12e635301ffbea1a84b1326ea6e908
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-06-22 15:32:57 +0200

    modified push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:44:37 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/06/06 16:23:05 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/06/22 12:26:03 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ typedef struct a_list
 }					t_stack;
 
 //utils:
-int		swap(t_stack **lst);
+int		swap(t_stack **lst, int fl);
 int		ss_swap(t_stack **a, t_stack **b);
-int		push(t_stack **lst1, t_stack **lst2);
-int		rotate(t_stack **lst);
+int		push(t_stack **lst1, t_stack **lst2, int fl);
+int		rotate(t_stack **lst, int fl);
 int		rr_rotate(t_stack **a, t_stack **b);
-int		reverse_rotate(t_stack **lst);
+int		reverse_rotate(t_stack **lst, int fl);
 int		rr_reverse_rotate(t_stack **a, t_stack **b);
 void	ft_lst_add_back(t_stack **lst, t_stack *new);
 void	ft_lst_add_front(t_stack **lst, t_stack *new);
@@ -41,15 +41,19 @@ int		ft_lst_ifsorted(t_stack **a);
 int		ft_checkerr(char **s, int fl);
 int		ft_lst_indexofmax(t_stack **lst);
 int		ft_lst_indexofmin(t_stack **lst);
+void	ft_lst_free(t_stack **lst);
 //sorting:
 int		sort(t_stack **a, t_stack **b);
 int		minisort(t_stack **a);
-int		find_smalltarget(t_stack **lst, int cnode);
 int		find_bigtarget(t_stack **lst, int cnode);
 int		costmovetop(t_stack **lst1, t_stack **lst2, int i_node, int i_target);
 int		movetop(t_stack **lst1, t_stack **lst2, int i_node, int i_target);
+int		nodetop(t_stack **lst, int i, int median, int fl);
+int		l_rotate(t_stack **lst, int i, int fl);
+int		l_reverse_rotate(t_stack **lst, int i, int fl);
+int		l_rr_rotate(t_stack **a, t_stack **b, int i);
+int		l_rr_reverse_rotate(t_stack **a, t_stack **b, int i);
 int		lsortab(t_stack **a, t_stack **b);
 int		lsortba(t_stack **a, t_stack **b);
-int		nodetop(t_stack **lst, int i, int median);
 
 #endif
